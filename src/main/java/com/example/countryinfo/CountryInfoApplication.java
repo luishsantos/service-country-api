3350323d5bfbde6dfd7b76d54e9305bdcbf77b95
luishsantos <luis-sobral-12@hotmail.com>
2023-11-18 01:10:15 +0000

    Fix CountryParser Update CountryInfoApplication @SpringBootApplication Annotation

--- src/main/java/com/example/countryinfo/CountryInfoApplication.java
+++ src/main/java/com/example/countryinfo/CountryInfoApplication.java
@@ -4,11 +4,10 @@ import com.example.countryinfo.service.CountryInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @RequiredArgsConstructor
 public class CountryInfoApplication {
 	private final CountryInfoService countryInfoService;
--- src/main/java/com/example/countryinfo/parser/CountryParser.java
+++ src/main/java/com/example/countryinfo/parser/CountryParser.java
@@ -1,21 +1,22 @@
 package com.example.countryinfo.parser;
 
-import java.util.List;
-import java.util.Map;
-
 import com.example.countryinfo.dto.CountryApiDTO;
 import com.example.countryinfo.dto.CountryDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface CountryParser {
-  @Mapping(target = "name", source ="name")
-  @Mapping(target = "capital", expression ="java(!countryApiDTO.getCapital().isEmpty() ? countryApiDTO.getCapital().get(0) : null)")
-  @Mapping(target = "language", expression ="java(!countryApiDTO.getLanguages().isEmpty() ? countryApiDTO.getLanguages().values().iterator().next() : null)")
-  @Mapping(target = "latitude", expression ="java(countryApiDTO.getLatlng().size() == 2 ? countryApiDTO.getLatlng().get(0) : null)")
-  @Mapping(target = "longitude", expression ="java(countryApiDTO.getLatlng().size() == 2 ? countryApiDTO.getLatlng().get(1) : null)")
-  @Mapping(target = "currency", expression ="java(parseCurrencies(countryApiDTO.getCurrencies()))")
+
+  @Mapping(target = "name", expression = "java(countryApiDTO.getName())")
+  @Mapping(target = "capital", expression = "java(!countryApiDTO.getCapital().isEmpty() ? countryApiDTO.getCapital().get(0) : null)")
+  @Mapping(target = "language", expression = "java(!countryApiDTO.getLanguages().isEmpty() ? countryApiDTO.getLanguages().values().iterator().next() : null)")
+  @Mapping(target = "latitude", expression = "java(countryApiDTO.getLatlng().size() == 2 ? countryApiDTO.getLatlng().get(0) : null)")
+  @Mapping(target = "longitude", expression = "java(countryApiDTO.getLatlng().size() == 2 ? countryApiDTO.getLatlng().get(1) : null)")
+  @Mapping(target = "currency", expression = "java(parseCurrencies(countryApiDTO.getCurrencies()))")
   CountryDTO toCountryDTO(CountryApiDTO countryApiDTO);
 
   List<CountryDTO> toCountryDTOList(List<CountryApiDTO> countryApiDTOList);
@@ -23,17 +24,14 @@ public interface CountryParser {
   default String parseCurrencies(Object currencies) {
     String currency = null;
 
-    if (currencies instanceof Iterable) {
-      for (Object item : (Iterable<?>) currencies) {
-        if (item instanceof Map) {
-          Map.Entry<?, ?> entry = ((Map<?, ?>) item).entrySet().iterator().next();
-          currency = entry.getValue().toString();
-
-        }
+    if (currencies instanceof Map) {
+      Map.Entry<?, ?> entry = ((Map<?, ?>) currencies).entrySet().iterator().next();
+      if (entry.getValue() instanceof Map) {
+        Map<?, ?> currencyInfo = (Map<?, ?>) entry.getValue();
+        currency = currencyInfo.get("name").toString();
       }
     }
     return currency;
   }
-
 
 }
