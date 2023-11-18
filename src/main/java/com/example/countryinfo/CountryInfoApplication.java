6ff59a1d6f8a4c015f9cfaf86483c7eb9da2159a
luishsantos <luis-sobral-12@hotmail.com>
2023-11-18 04:29:32 +0000

    Change pom to .war packaging add .war and .jar files

--- src/main/java/com/example/countryinfo/CountryInfoApplication.java
+++ src/main/java/com/example/countryinfo/CountryInfoApplication.java
@@ -4,16 +4,22 @@ import com.example.countryinfo.service.CountryInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class CountryInfoApplication {
+public class CountryInfoApplication extends SpringBootServletInitializer {
   private final CountryInfoService countryInfoService;
 
   public static void main(String[] args) {
     SpringApplication.run(CountryInfoApplication.class, args);
+  }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(CountryInfoApplication.class);
   }
 
   @EventListener(ApplicationReadyEvent.class)
